e1fd280db40c58dda98e5959634bba8803475f69
sergiogval <sergioval@gmail.com>
2021-12-20 23:34:50 -0600

    Add skeleton of the game

---
+++ src/app/core/splash-screen.js
@@ -0,0 +1,12 @@
+class SplashScreen {
+  render() {
+  // create a DIV
+  const splashElement = document.createElement('div')
+  debugger;
+  // Add it to the index.html
+  document.body.appendChild(splashElement);
+  // Set the background to render the assets
+  // Image that we chose for this screen!
+  }
+  destroy() {}
+}

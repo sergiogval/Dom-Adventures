4cd3f668e655a52b156fed8871b8eb3df9cf0858
sergiogval <sergioval@gmail.com>
2021-12-22 02:49:06 -0600

    Add a class with JS

--- src/app/core/splash-screen.js
+++ src/app/core/splash-screen.js
@@ -1,12 +1,27 @@
+
+
 class SplashScreen {
   render() {
-  // create a DIV
-  const splashElement = document.createElement('div')
-  debugger;
+    const styleElement = document.createElement('style');
+    styleElement.innerHTML = `
+      .splash-screen {
+        background: url('../assets/splash-screen.jpg');
+        width: 650px;
+        height: 650px;
+      }
+    `;
+    document.head.appendChild(styleElement);
+    const splashElement = document.createElement('div')
+    const rootElement = document.getElementById("root");
+    rootElement.appendChild(splashElement);
+
+    splashElement.classList.add('splash-screen');
+
   // Add it to the index.html
-  document.body.appendChild(splashElement);
+
   // Set the background to render the assets
   // Image that we chose for this screen!
+    splashElement.classList.add('splash-screen');
   }
   destroy() {}
 }

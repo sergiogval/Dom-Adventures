3dca6f0f9010b9343c5f5d6b28a7d483cbe48cb3
sergiogval <sergioval@gmail.com>
2021-12-26 09:49:36 -0600

    Add splash-screen animation

--- src/app/core/splash-screen.js
+++ src/app/core/splash-screen.js
@@ -1,27 +1,57 @@
-
-
+const MAX_FRAMES = 2;
+const HALF_SECOND = 500;
 class SplashScreen {
+  #splashElement = document.createElement('div')
+  #animation = null;
+
   render() {
     const styleElement = document.createElement('style');
     styleElement.innerHTML = `
       .splash-screen {
-        background: url('../assets/splash-screen.jpg');
+        background: url('../assets/splash-screen.jpg') no-repeat;
         width: 650px;
         height: 650px;
       }
+
+      .splash-screen-1 {
+        background-position-x: 0;
+      }
+
+      .splash-screen-2 {
+        background-position-x: -650px;
+      }
     `;
     document.head.appendChild(styleElement);
-    const splashElement = document.createElement('div')
+
+    this.#splashElement.classList.add('splash-screen', 'splash-screen-1');
+
     const rootElement = document.getElementById("root");
-    rootElement.appendChild(splashElement);
+    rootElement.appendChild(this.#splashElement);
 
-    splashElement.classList.add('splash-screen');
-
-  // Add it to the index.html
-
-  // Set the background to render the assets
-  // Image that we chose for this screen!
-    splashElement.classList.add('splash-screen');
+    this.#animate();
   }
-  destroy() {}
-}
+
+  destroy() {
+    this.#splashElement.remove();
+    window.clearInterval(this.#animation);
+  }
+
+  #animate() {
+    let frame = 1;
+
+
+    const animate = () => {
+      this.#splashElement.classList.remove(`splash-screen-${frame}`);
+
+      frame++;
+
+      if (frame > MAX_FRAMES) {
+        frame = 1;
+      }
+
+      this.#splashElement.classList.add(`splash-screen-${frame}`);
+    };
+
+    this.#animation = setInterval(animate, HALF_SECOND);
+  }
+};
